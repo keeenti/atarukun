ba0bb39beef484936c96d547bfd63773478413ca
宗一郎 徳永 <ikkun11_06@yahoo.co.jp>
2021-04-11 20:29:31 +0900

    登録画面を修正

--- atarukun/Touroku/Touroku.cs
+++ atarukun/Touroku/Touroku.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,61 @@ namespace atarukun.Touroku
         public Touroku()
         {
             InitializeComponent();
+        }
+
+        //登録ボタン押下時のメソッド
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // 変数
+            string filepath = "D:\\git\\atarukun\\atarukun\\DB\\TousenNumber.txt";
+
+            // 文字コードを指定
+            Encoding enc = Encoding.GetEncoding("Shift_JIS");
+
+            // ファイルを開く
+            StreamWriter writer = new StreamWriter(filepath, false, enc);
+
+            // テキストを書き込む
+            writer.WriteLine(textBox1.Text);
+
+            // ファイルを閉じる
+            writer.Close();
+
+            //メッセージボックスを表示
+            MessageBox.Show("登録しました",
+               "登録完了",
+               MessageBoxButtons.OK,
+               MessageBoxIcon.None);
+        }
+
+        //昨日の当選番号を確認ボタン押下時のメソッド
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 変数
+            string filepath = "D:\\git\\atarukun\\atarukun\\DB\\TousenNumber.txt";
+
+
+            // 文字コードを指定してファイルを開く
+            StreamReader sr = new StreamReader(filepath, Encoding.GetEncoding("Shift_JIS"));
+
+            //文字を読み込む
+            string str = sr.ReadToEnd();
+
+            //テキストボックスにTousenNumber.txtの内容を呼び出す
+            textBox1.Text = str;
+
+            //ファイルを閉じる
+            sr.Close();
+
+            Console.WriteLine(str);
+
+            //メッセージボックスを表示
+            MessageBox.Show("昨日の当選番号を呼び出しました",
+               "呼び出し完了",
+               MessageBoxButtons.OK,
+               MessageBoxIcon.None);
+
         }
     }
 }
