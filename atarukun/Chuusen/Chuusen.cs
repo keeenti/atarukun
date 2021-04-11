26e2cae89808d52450d5be22a7b4ea33edbf012d
keeenti <minzoku0514@gmail.com>
2021-04-11 17:43:15 +0900

    画面修正

---
+++ atarukun/Chuusen/Chuusen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atarukun.Chuusen
+{
+    //抽選結果クラス
+    class Chuusen
+    {
+        //抽選ボタン押下時のメソッド
+        public string Chuusen_Click()
+        {
+            //抽選結果を格納する変数
+            string Chuusen_result;
+
+            //仮
+            Chuusen_result = "100";
+
+            //抽選結果を戻り値として返却する
+            return Chuusen_result;
+
+        }
+
+    }
+}
