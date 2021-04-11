51201d2e152bd660cb5d97252c88c393612659fa
宗一郎 徳永 <ikkun11_06@yahoo.co.jp>
2021-04-11 21:09:14 +0900

    登録画面修正

--- atarukun/Touroku/Touroku.Designer.cs
+++ atarukun/Touroku/Touroku.Designer.cs
@@ -31,66 +31,61 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(672, 12);
+            this.button2.Location = new System.Drawing.Point(896, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 44);
+            this.button2.Size = new System.Drawing.Size(135, 55);
             this.button2.TabIndex = 4;
             this.button2.Text = "閉じる";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(501, 92);
+            this.button1.Location = new System.Drawing.Point(547, 115);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(169, 64);
+            this.button1.Size = new System.Drawing.Size(225, 80);
             this.button1.TabIndex = 5;
             this.button1.Text = "登録する";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(90, 92);
+            this.button3.Location = new System.Drawing.Point(241, 115);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(169, 64);
+            this.button3.Size = new System.Drawing.Size(225, 80);
             this.button3.TabIndex = 6;
             this.button3.Text = "昨日の当選番号を確認";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(298, 92);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(169, 64);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "値を変更する";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(270, 203);
+            this.textBox1.Location = new System.Drawing.Point(360, 254);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(227, 40);
+            this.textBox1.Size = new System.Drawing.Size(301, 49);
             this.textBox1.TabIndex = 8;
             // 
             // Touroku
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 562);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Touroku";
             this.Text = "Touroku";
             this.ResumeLayout(false);
@@ -103,7 +98,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
