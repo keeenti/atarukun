7ca7ca94bfbcb257a175554d185cbd0d3c6b0c1a
宗一郎 徳永 <ikkun11_06@yahoo.co.jp>
2021-05-08 21:46:40 +0900

    登録画面修正

--- atarukun/Touroku/Touroku.Designer.cs
+++ atarukun/Touroku/Touroku.Designer.cs
@@ -32,12 +32,17 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(896, 15);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(135, 55);
             this.button2.TabIndex = 4;
@@ -47,7 +52,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(547, 115);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(225, 80);
             this.button1.TabIndex = 5;
@@ -58,7 +63,7 @@
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(241, 115);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(225, 80);
             this.button3.TabIndex = 6;
@@ -68,12 +73,57 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(360, 254);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(241, 256);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(301, 49);
+            this.textBox1.Size = new System.Drawing.Size(161, 57);
             this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(426, 256);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 57);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(611, 256);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(161, 57);
+            this.textBox3.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "抽選回";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(471, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "抽選年月日";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(658, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "当選番号";
             // 
             // Touroku
             // 
@@ -81,11 +131,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Touroku";
             this.Text = "Touroku";
             this.ResumeLayout(false);
@@ -99,5 +154,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- atarukun/Touroku/Touroku.cs
+++ atarukun/Touroku/Touroku.cs
@@ -22,12 +22,48 @@ namespace atarukun.Touroku
         private void button1_Click(object sender, EventArgs e)
         {
 
-            string hantei;
-            hantei = textBox1.Text;
-            hantei = hantei.Replace("\r", "").Replace("\n", "");
+            string TousenKai;
+            string Tousendate;
+            string TousenBangou;
 
-            //判定結果がfalseだった場合
+            //抽選回のテキストボックスを取得
+            TousenKai = textBox1.Text;
+            TousenKai = TousenKai.Replace("\r", "").Replace("\n", "");
+
+            //抽選年月日のテキストボックスを取得
+            Tousendate = textBox2.Text;
+            Tousendate = Tousendate.Replace("\r", "").Replace("\n", "");
+
+            //当選番号のテキストボックスを取得
+            TousenBangou = textBox3.Text;
+            TousenBangou = TousenBangou.Replace("\r", "").Replace("\n", "");
+
+            //抽選回の判定結果がfalseだった場合
             if (IsNumeric(textBox1.Text) == false)
+            {
+                //メッセージボックスを表示する
+                MessageBox.Show("四桁の数字を入力してください。",
+                    "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                return;
+            }
+
+            //抽選年月日の判定結果がfalseだった場合
+            if (IsDate(textBox2.Text) == false)
+            {
+                //メッセージボックスを表示する
+                MessageBox.Show("「yyyy/mm/dd」の形式で入力してください。",
+                    "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                return;
+            }
+
+            //当選番号の判定結果がfalseだった場合
+            if (IsNumeric(textBox3.Text) == false)
             {
                 //メッセージボックスを表示する
                 MessageBox.Show("三桁の数字を入力してください。",
@@ -38,7 +74,7 @@ namespace atarukun.Touroku
                 return;
             }
 
-            if (hantei.Length == 3)
+            if (TousenBangou.Length == 3 & TousenKai.Length == 4)
             {
                 // 文字コードを指定
                 Encoding enc = Encoding.GetEncoding("Shift_JIS");
@@ -61,16 +97,16 @@ namespace atarukun.Touroku
                 sr.Close();
 
                 //変数Tousenのもっとも古い数値以外を抜き出す
-                Tousen = Tousen.Substring(4);
+               //Tousen = Tousen.Substring(4);
 
-                //変数Tousenにテキストボックスの内容を書き加える
-                Tousen = Tousen + "," + textBox1.Text; 
+                //変数Tousenにテキストボックスの値を書き加える
+                Tousen = textBox1.Text + "," + textBox2.Text + "," + textBox3.Text + Environment.NewLine + Tousen; 
 
                 //ファイルを開く
                 StreamWriter writer = new StreamWriter(filepath, false, enc);
 
                 // テキストを書き込む
-                writer.WriteLine(Tousen);
+                writer.Write(Tousen);
 
                 // ファイルを閉じる
                 writer.Close();
@@ -81,11 +117,10 @@ namespace atarukun.Touroku
                    MessageBoxButtons.OK,
                    MessageBoxIcon.None);
             }
-
             else
             {
                 //メッセージボックスを表示する
-                MessageBox.Show("三桁の数字を入力してください。",
+                MessageBox.Show("正しい値を入力してください。",
                     "エラー",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
@@ -108,8 +143,14 @@ namespace atarukun.Touroku
             //文字を読み込む
             string str = sr.ReadToEnd();
 
-            //テキストボックスにTousenNumber.txtの内容を呼び出す
-            textBox1.Text = str;
+            //抽選回にTousenNumber.txtの内容を呼び出す
+            textBox1.Text = str.Substring(0, 4); 
+
+            //抽選年月日にTousenNumber.txtの内容を呼び出す
+            textBox2.Text = str.Substring(5, 10);
+
+            //当選番号にTousenNumber.txtの内容を呼び出す
+            textBox3.Text = str.Substring(16, 3);
 
             //ファイルを閉じる
             sr.Close();
@@ -135,6 +176,64 @@ namespace atarukun.Touroku
                 null,
                 out dNullable
             );
+        }
+
+        //当選年月日が適正かどうか判定するメソッド
+        public static bool IsDate(string stDate)
+        {
+            try
+            {
+                //西暦の変数
+                string DateHantei1;
+                //月の変数
+                string DateHantei2;
+                //日の変数
+                string DateHantei3;
+                //日付区切りに"/"の変数
+                string DateSlashHantei1;
+                string DateSlashHantei2;
+
+                //西暦の切り出し
+                DateHantei1 = stDate.Substring(0, 4);
+                //月の切り出し
+                DateHantei2 = stDate.Substring(5, 2);
+                //日の切り出し
+                DateHantei3 = stDate.Substring(8, 2);
+                //日付区切りの"/"の切り出し
+                DateSlashHantei1 = stDate.Substring(4, 1);
+                DateSlashHantei2 = stDate.Substring(7, 1);
+
+                if (stDate.Length != 10)
+                {
+                    return false;
+                }
+                else if (DateSlashHantei1 != "/" & DateSlashHantei2 != "/")
+                {
+                    return false;
+
+                }
+                else if (DateHantei1.Length != 4 & DateHantei2.Length != 2 & DateHantei3.Length != 2)
+                {
+                    return false;
+                }
+                else if (IsNumeric(DateHantei1) == false & IsNumeric(DateHantei2) == false & IsNumeric(DateHantei3) == false)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (ArgumentOutOfRangeException e) 
+            {
+                MessageBox.Show("例外が発生しました。",
+                    "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+                return false;
+              }
         }
     }
     
